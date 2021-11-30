8dbaa01ce22d2c2ae0e40593cc3f13669bd3c7c1
Rayhan <nofussdufus@gmail.com>
2021-11-30 22:03:43 +0000

    Add linters

--- src/index.js
+++ src/index.js
@@ -1,58 +1,57 @@
-import _ from 'lodash';
 import './style.css';
 
 const toDoItems = [
   {
-    description:'take out trash',
-    completed:false,
-    index:0
+    description: 'take out trash',
+    completed: false,
+    index: 0,
   },
   {
-    description:'wash dishes',
-    completed:false,
-    index:1
+    description: 'wash dishes',
+    completed: false,
+    index: 1,
   },
   {
-    description:'walk dog',
-    completed:false,
-    index:2
-  }
+    description: 'walk dog',
+    completed: false,
+    index: 2,
+  },
 ];
 
 function component() {
   const element = document.createElement('ul');
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
   const refresh = document.createElement('li');
   refresh.innerHTML = `
     <p class="header">Today's To Do</p>
-    <button class="button">|</button>
+    <button class="button"><i class="fas fa-sync"></i></button>
   `;
   element.appendChild(refresh);
 
   const add = document.createElement('li');
-  add.classList.add('inputNew')
+  add.classList.add('inputNew');
   add.innerHTML = `
     <input placeholder="Add to your list..."> 
-    <button class="button">|</button>
+    <button class="button"><i class="fas fa-reply"></i></button>
   `;
   element.appendChild(add);
 
   toDoItems.forEach((item) => {
-    let listElement = document.createElement('li');
+    const listElement = document.createElement('li');
     listElement.innerHTML = `
      <input type="checkbox" class="checkbox">
      <p class="title">${item.description}</p> 
-     <button class="button">|</button>
+     <button class="button"><i class="fas fa-ellipsis-v"></i></button>
     `;
-    listElement.classList.add('toDoItem')
+
+    // <i class="far fa-trash-alt"></i> - trash.delete icon
+    listElement.classList.add('toDoItem');
     element.appendChild(listElement);
   });
 
-  const clearCompleted = document.createElement('li')
-  clearCompleted.classList.add('clearCompleted')
-  clearCompleted.innerHTML = '<button>Clear all completed</button>'
-  element.appendChild(clearCompleted)
+  const clearCompleted = document.createElement('li');
+  clearCompleted.classList.add('clearCompleted');
+  clearCompleted.innerHTML = '<button>Clear all completed</button>';
+  element.appendChild(clearCompleted);
 
   return element;
 }
