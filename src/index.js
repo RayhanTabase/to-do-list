3aaa1355a2256ad5acd1750e03656a028f288d52
Rayhan <nofussdufus@gmail.com>
2021-11-30 18:13:05 +0000

    Add webpack, index template, dynamic population of to do list

--- src/index.js
+++ src/index.js
@@ -1,2 +1,50 @@
 import _ from 'lodash';
 import './style.css';
+
+const toDoItems = [
+  {
+    description:'take out trash',
+    completed:false,
+    index:1
+  },
+  {
+    description:'wash dishes',
+    completed:false,
+    index:2
+  },
+  {
+    description:'walk dog',
+    completed:false,
+    index:2
+  }
+];
+
+function component() {
+  const element = document.createElement('ul');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  const refresh = document.createElement('li');
+  refresh.innerHTML = "<p>Today's To Do</p>";
+  element.appendChild(refresh);
+
+  const add = document.createElement('li');
+  add.innerHTML = '<input placeholder="Add to your list...">';
+  element.appendChild(add);
+
+
+
+  toDoItems.forEach((item) => {
+    let listElement = document.createElement('li');
+    listElement.innerHTML = `<input type="checkbox"> <p>${item.description}</p>`;
+    listElement.classList.add('toDoItem')
+    element.appendChild(listElement);
+  });
+
+  const clearCompleted = document.createElement('li')
+  clearCompleted.innerHTML = '<button>Clear all completed</button>'
+  element.appendChild(clearCompleted)
+
+  return element;
+}
+
+document.querySelector('#toDoListContainer').appendChild(component());
