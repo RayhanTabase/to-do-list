75b8058fd9ad5a9d4190c762fa4fcc67a9e32685
Rayhan <nofussdufus@gmail.com>
2021-12-03 17:16:33 +0000

    Add CRUD functinality to to-do list

---
+++ src/html.js
@@ -0,0 +1,132 @@
+import ToDoList from './toDo.js';
+import LocalStorage from './storage.js';
+
+export default class HtmlToDo {
+   // Edit mode
+   static toggleEditable(listElement){
+    document.querySelectorAll('.toDoItem').forEach((element)=>{
+      let edit = element.querySelector('.item-description')
+      let deleteBtn = element.querySelector('.delete-button')
+      let moveBtn = element.querySelector('.move-button')
+
+      if(element === listElement){
+        deleteBtn.style.display = "block"
+        moveBtn.style.display = "none"
+        edit.readOnly = false
+        element.classList.add('editing')
+      }else{
+        deleteBtn.style.display = "none"
+        moveBtn.style.display = "block"
+        edit.readOnly = true
+        element.classList.remove('editing')
+      }
+    })
+  }
+ 
+  // Create list element
+  static createListItem(item) {
+    const listElement = document.createElement('div');
+    listElement.classList.add('toDoItem');
+    listElement.classList.add('container-list');
+    listElement.style.order = item.index;
+
+    let crossed = '';
+    if(item.completed){
+      crossed = 'crossed';
+    }
+
+    listElement.innerHTML = `
+     <input type="checkbox" class="checkbox">
+     <form class="edit-input"> <input type="text" name="title" class="item-description ${crossed}" value=${item.description} readonly></form>
+     <button class="button move-button"><i class="fas fa-ellipsis-v"></i></button>
+     <button class="button delete-button"><i class="fas fa-trash-alt"></i></button>
+    `;
+
+    // Add  status changer
+    const completed = listElement.querySelector('.checkbox');
+    completed.checked = item.completed;
+    completed.addEventListener('change', (e) => {
+      ToDoList.changeStatus(item.index, e.target.checked);
+      listElement.querySelector('.item-description').classList.toggle('crossed');
+    });
+
+    // Add delete feature
+    const deleteBtn = listElement.querySelector('.delete-button');
+    deleteBtn.addEventListener('click',()=>{
+      ToDoList.delete(item.index);
+    })
+    
+    // Handle edit form
+    const editForm = listElement.querySelector('.edit-input');
+    editForm.addEventListener('submit', (e)=>{
+      e.preventDefault();
+      let data = new FormData(editForm);
+      let newName = data.get('title');
+      ToDoList.update(newName,item.index);
+    })
+
+    // Toggle edit
+    listElement.querySelector('.item-description').addEventListener('click', ()=> HtmlToDo.toggleEditable(listElement));
+
+    return listElement;
+  }
+
+  // Create initial to-do list structure
+  static createStructure() {
+    const element = document.createElement('div');
+    element.className = 'toDoListContainer'
+
+    // Create header section
+    const header = document.createElement('div');
+    header.classList.add('container-list')
+    header.innerHTML = `
+      <p class="header">Today's To Do</p>
+      <button class="button"><i class="fas fa-sync"></i></button>
+    `;
+    header.style.order =  0;
+    element.appendChild(header);
+
+    // Create add section
+    const add = document.createElement('form');
+    add.classList.add('inputNew');
+    add.classList.add('container-list');
+
+    add.innerHTML = `
+      <input placeholder="Add to your list..." name="title" id="addToListInput"> 
+      <button type="submit" class="button"><i class="fas fa-reply"></i></button>
+    `;
+    add.style.order = 1;
+    add.addEventListener('submit',(e)=> {
+      e.preventDefault()
+      let data = new FormData(add);
+      let newItem = data.get('title')
+      ToDoList.add(newItem) 
+      add.reset();
+    })
+    element.appendChild(add);
+
+    // Create list ul
+    let listUl = document.createElement('div')
+    listUl.classList.add('toDoList')
+    listUl.style.order = 2;
+    element.appendChild(listUl)
+
+    // Add clear completed
+    const clearCompleted = document.createElement('div');
+    clearCompleted.classList.add('clearCompleted');
+    clearCompleted.style.order = 3;
+    clearCompleted.innerHTML = '<button>Clear all completed</button>';
+    clearCompleted.querySelector('button').addEventListener('click', ()=>ToDoList.deleteCompleted())
+    element.appendChild(clearCompleted);
+
+    // Create list items and append
+    LocalStorage.getList().forEach((item) => {
+      listUl.append(HtmlToDo.createListItem(item));
+    });
+
+    // Append to main content
+    const container = document.querySelector('#main')
+    container.innerHTML = ""; 
+    return container.appendChild(element);
+  }
+}
--- src/toDo.js
+++ src/toDo.js
@@ -1,11 +1,71 @@
 import LocalStorage from './storage.js';
+import HtmlToDo from './html.js';
+
+class listItem {
+  constructor(description,index) {
+    this.description = description;
+    this.index = index;
+    this.completed = false;
+  }
+}
 
 export default class ToDoList {
   // change status(completed) of to-do item
   static changeStatus(index, status) {
-    const model = LocalStorage.getList();
-    const listItem = model.find((item) => item.index === index);
+    let model = LocalStorage.getList();
+    let listItem = model.find((item) => item.index === index);
     listItem.completed = status;
     LocalStorage.save(model);
+  }
+
+  // Add 
+  static add(newInput) {
+    // Add if input is not empty
+    if(newInput.trim().length > 1){
+      let model = LocalStorage.getList();
+      let new_index = model.length + 1;
+      let newItem = new listItem(newInput,new_index);
+      model.push(newItem);
+      LocalStorage.save(model);
+      
+      // Add new item to the DOM
+      let listUl = document.querySelector('.toDoList')
+      listUl.appendChild(HtmlToDo.createListItem(newItem));
+    }
+  }
+
+  // Update
+  static update(description , index) {
+    let model = LocalStorage.getList();
+    let listItem = model.find((item) => item.index === index);
+    listItem.description = description;
+    LocalStorage.save(model);
+    HtmlToDo.toggleEditable()
+  }
+
+  // Delete
+  static delete(index) {
+    let model = LocalStorage.getList();
+    let newModel = model.filter((item) => item.index !== index);
+    newModel = this.reAssignIndex(newModel);
+    LocalStorage.save(newModel);
+    HtmlToDo.createStructure()
+  }
+
+  // Delete completed
+  static deleteCompleted() {
+    let model = LocalStorage.getList();
+    let newModel = model.filter((item) => item.completed === false);
+    newModel = this.reAssignIndex(newModel);
+    LocalStorage.save(newModel);
+    HtmlToDo.createStructure()
+  }
+
+  // Adjusts list index values to match actual index 
+  static reAssignIndex(model) {
+    model.forEach((item,index) => {
+      item.index = index + 1;
+    });
+    return model;
   }
 }
